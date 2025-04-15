41187ad8f410bf37896cfedf3ad98350a24bcd67
danielantonio14 <danielantonio.dad52@gmail.com>
2025-04-15 13:18:21 +0100

    auth, users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Agendamento } from './agendamento/agendamento.entity';
 import { AgendamentosModule } from './agendamento/agendamentos.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -18,6 +20,8 @@ import { AgendamentosModule } from './agendamento/agendamentos.module';
     }),
     TypeOrmModule.forFeature([Agendamento]),
     AgendamentosModule,
+    AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
